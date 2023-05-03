3cf564a9636a7e79629733ec85456de457e9efe6
AleksRG <aleksandar.kystendil.georgiev@gmail.com>
2023-05-03 19:34:26 +0300

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,23 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { createTheme, NextUIProvider } from "@nextui-org/react";
+
+// 2. Call `createTheme` and pass your custom theme values
+const theme = createTheme({
+  type: "dark", // it could be "light" or "dark"
+  theme: {
+    colors: {
+      primary: "#4ADE7B",
+      secondary: "#F9CB80",
+      error: "#FCC5D8",
+    },
+  },
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <NextUIProvider theme={theme}>
+      <Component {...pageProps} />
+    </NextUIProvider>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,118 +1,230 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Image from "next/image";
+import React from "react";
+import { Inter } from "next/font/google";
+import { User, Radio, Input } from "@nextui-org/react";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      className={`flex min-h-screen flex-col space-y-4  items-center  p-4 ${inter.className}`}
     >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      <div className="flex  items-center justify- md:space-y-0 flex-wrap gap-2 w-full max-w-7xl">
+        <div className="border-neutral-800 dark:from-inherit lg:static rounded-xl border px-4 py-1.5 space-x-2 bg-zinc-800/30 flex w-28 hover:border-fuchsia-950 ">
+          <img
+            src="https://raw.githubusercontent.com/solana-labs/token-list/main/assets/mainnet/So11111111111111111111111111111111111111112/logo.png"
+            className="w-6 h-6 rounded-xl"
+          />
+          <button className="font-mono font-bold text-white/50">$21.32</button>
+        </div>{" "}
+        <div className="border-neutral-800 dark:from-inherit lg:static w-28 rounded-xl border px-4 py-1.5 space-x-2 bg-zinc-800/30 flex hover:border-fuchsia-950 ">
+          <img
+            src="https://raw.githubusercontent.com/solana-labs/token-list/main/assets/mainnet/EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v/logo.png"
+            className="w-6 h-6 rounded-xl"
+          />
+          <button className="font-mono font-bold text-white/50">$1.00</button>
         </div>
       </div>
+      <div className="flex space-y-4 items-center justify-between md:space-y-0 flex-wrap w-full max-w-7xl">
+        {" "}
+        <div className="z-10 w-full md:max-w-[69%] items-center px-4 py-2 justify-between font-mono text-sm flex flex-col border-neutral-800 from-inherit hover:border-fuchsia-950 lg:static rounded-xl border bg-zinc-800/30 space-y-2">
+          <div className="w-full flex justify-between items-start ">
+            {" "}
+            <text className="text-xl font-bold">ATX / USDC</text>{" "}
+            <div className="border-neutral-800 dark:from-inherit lg:static w-fit rounded-xl border px-4 py-1.5 bg-zinc-800/30">
+              <button className="font-mono font-bold">BWWz..EE4S</button>
+            </div>
+          </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+          <div className="flex justify-between w-full">
+            <div className="flex flex-col">
+              <span>AquaDEX protocol Solana blockchain </span>
+              <span className="text-white/50">
+                Fee: 0.0 bps - Rebate: 0.0 bps
+              </span>
+            </div>
+          </div>
+          <div className="flex flex-wrap items-center w-full gap-2">
+            <div className="flex justify-center items-center space-x-2">
+              {" "}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-8 h-8 text-fuchsia-950"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M2.25 18.75a60.07 60.07 0 0115.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 013 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 00-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 01-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 003 15h-.75M15 10.5a3 3 0 11-6 0 3 3 0 016 0zm3 0h.008v.008H18V10.5zm-12 0h.008v.008H6V10.5z"
+                />
+              </svg>
+              <div className="flex flex-col">
+                <span>Last Price</span>
+                <span className="text-white/50">0.70000</span>
+              </div>
+            </div>
+            <div className="flex justify-center items-center space-x-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-8 h-8 text-fuchsia-950"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z"
+                />
+              </svg>
+
+              <div className="flex flex-col">
+                <span>Last Trade</span>
+                <span className="text-white/50">02.05.2023/07:21:43</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="z-10 w-full md:max-w-[30%] items-center px-4 py-2 justify-between font-mono text-sm flex flex-col border-neutral-800 from-inherit lg:static rounded-xl border bg-zinc-800/30 space-y-5 hover:border-fuchsia-950">
+          <div className="w-full  flex justify-between items-start ">
+            {" "}
+            <text className="text-xl ">Balances</text>{" "}
+          </div>
+
+          <div className="flex flex-wrap items-center w-full gap-2">
+            <User
+              bordered
+              src="https://media.atellix.net/token/atx.png"
+              name="Atellix / ATX"
+            >
+              <button className="text-fuchsia-800 text-xs">
+                CREATTE TOKEN ACCOUNT
+              </button>
+            </User>
+            <User
+              bordered
+              src="https://s2.coinmarketcap.com/static/img/coins/128x128/3408.png"
+              name="USD Coin / USDC"
+            >
+              <button className="text-fuchsia-800 text-xs">
+                CREATTE TOKEN ACCOUNT
+              </button>
+            </User>
+          </div>
+        </div>
+      </div>{" "}
+      <div className="flex my-4 items-center justify-center md:space-y-0 flex-wrap w-full max-w-7xl border-neutral-800 from-inherit hover:border-fuchsia-950 lg:static rounded-xl border bg-zinc-800/30 h-96">
+        {" "}
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="flex items-start justify-center flex-col w-full max-w-7xl hover:border-fuchsia-950 border-neutral-800 from-inherit lg:static rounded-xl border bg-zinc-800/30 h-fit font-mono px-4 py-2 space-y-4">
+        {" "}
+        <span className="text-xl">BID</span>
+        <Radio.Group orientation="horizontal" defaultValue="sm">
+          <Radio value="sm" size="sm" color="primary">
+            Limit Bid
+          </Radio>
+          <Radio value="secondary" size="sm" color="primary">
+            Market Bid
+          </Radio>
+        </Radio.Group>
+        <div className="w-full flex flex-wrap items-center gap-2">
+          {" "}
+          <Input placeholder="Bid Quantity" />
+          <Input placeholder="Bid Quantity" />
+          <div className="border-neutral-800 dark:from-inherit lg:static w-fit rounded-xl border px-4 py-1.5 bg-zinc-800/30">
+            <button className="font-mono font-bold hover:text-fuchsia-800">
+              Place BID
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="flex items-center justify-center md:space-y-0 flex-col w-full hover:border-fuchsia-950 max-w-7xl border-neutral-800 from-inherit lg:static rounded-xl border bg-zinc-800/30 h-fit font-mono text-xs">
+        <div className="w-full flex justify-around px-4 py-2 text-fuchsia-800">
+          {" "}
+          <span className="">Order</span>
+          <span>Taker</span>
+          <span>Maker</span>
+          <span>Timestamp</span>
+          <span>Quantity</span>
+          <span>Price</span>
+        </div>
+        <div className="border-t-[0.5px] border-neutral-800 w-full flex  justify-around px-4 py-2 hover:bg-neutral-800/70">
+          {" "}
+          <span className="">Bid</span>
+          <span className="">ALy..FE6</span>
+          <span className="">4RD..tbU</span>
+          <span className="">2.05.2023 </span>
+          <span className="">5.00</span>
+          <span className="">0.7000</span>
+        </div>{" "}
+        <div className="border-t-[0.5px] border-neutral-800 w-full flex  justify-around px-4 py-2 hover:bg-neutral-800/70">
+          {" "}
+          <span className="">Bid</span>
+          <span className="">ALy..FE6</span>
+          <span className="">4RD..tbU</span>
+          <span className="">2.05.2023 </span>
+          <span className="">5.00</span>
+          <span className="">0.7000</span>
+        </div>{" "}
+        <div className="border-t-[0.5px] border-neutral-800 w-full flex  justify-around px-4 py-2 hover:bg-neutral-800/70">
+          {" "}
+          <span className="">Bid</span>
+          <span className="">ALy..FE6</span>
+          <span className="">4RD..tbU</span>
+          <span className="">2.05.2023 </span>
+          <span className="">5.00</span>
+          <span className="">0.7000</span>
+        </div>{" "}
+        <div className="border-t-[0.5px] border-neutral-800 w-full flex  justify-around px-4 py-2 hover:bg-neutral-800/70">
+          {" "}
+          <span className="">Bid</span>
+          <span className="">ALy..FE6</span>
+          <span className="">4RD..tbU</span>
+          <span className="">2.05.2023 </span>
+          <span className="">5.00</span>
+          <span className="">0.7000</span>
+        </div>{" "}
+        <div className="border-t-[0.5px] border-neutral-800 w-full flex  justify-around px-4 py-2 hover:bg-neutral-800/70">
+          {" "}
+          <span className="">Bid</span>
+          <span className="">ALy..FE6</span>
+          <span className="">4RD..tbU</span>
+          <span className="">2.05.2023 </span>
+          <span className="">5.00</span>
+          <span className="">0.7000</span>
+        </div>{" "}
+        <div className="border-t-[0.5px] border-neutral-800 w-full flex  justify-around px-4 py-2 hover:bg-neutral-800/70">
+          {" "}
+          <span className="">Bid</span>
+          <span className="">ALy..FE6</span>
+          <span className="">4RD..tbU</span>
+          <span className="">2.05.2023 </span>
+          <span className="">5.00</span>
+          <span className="">0.7000</span>
+        </div>{" "}
+        <div className="border-t-[0.5px] border-neutral-800 w-full flex  justify-around px-4 py-2 hover:bg-neutral-800/70">
+          {" "}
+          <span className="">Bid</span>
+          <span className="">ALy..FE6</span>
+          <span className="">4RD..tbU</span>
+          <span className="">2.05.2023 </span>
+          <span className="">5.00</span>
+          <span className="">0.7000</span>
+        </div>{" "}
+      </div>
+      <div className="font-mono text-sm flex justify-between w-full max-w-7xl px-1">
+        <span>© 2023 Atellix, Inc.</span>
+        <div className="flex space-x-4">
+          {" "}
+          <button className="hover:text-fuchsia-800">Terms</button>
+          <button className="hover:text-fuchsia-800">MIT Licence</button>
+        </div>
       </div>
     </main>
-  )
+  );
 }
